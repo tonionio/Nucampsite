51c683d02ac4a5695db9a2d64e74e6a0081a00e3
Tonionio <aam3@live.com>
2023-08-27 18:50:02 -0700

    Updated the website and fixed the weather widget issue

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,34 @@ carouselButton.addEventListener('click', function() {
         carousel.cycle();
     }
 });
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = "Seattle"; // Replace with your desired city
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+    }
+}
+
+function displayWeather(data) {
+    const weatherIconCode = data.weather[0].icon;
+    const weatherTemp = data.main.temp;
+    const weatherDescription = data.weather[0].description;
+
+    const weatherIcon = document.createElement('img');
+    weatherIcon.src = `https://openweathermap.org/img/w/${weatherIconCode}.png`;
+    document.getElementById('weather-icon').appendChild(weatherIcon);
+
+    document.getElementById('weather-temp').textContent = `${weatherTemp}\u00B0 F`;
+    document.getElementById('weather-description').textContent = weatherDescription;
+}
+
+// Call the fetchWeather function and then display the weather data
+fetchWeather().then(data => displayWeather(data));
